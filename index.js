3911ea73f89f3e7449c4b74994b71278ec229430
Rashedul Hasan <Rashedul Hasan>
2022-03-23 03:09:03 +0600

    JS-Class-02

---
+++ index.js
@@ -0,0 +1,46 @@
+// var firstName = "Rashedul";
+// var lastName  = "Hasan";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+// var num = Math.abs(-234);
+// console.log(num);
+
+// var num2 = Math.round(.499);
+// console.log(num2);
+
+// var num3 = Math.round(.5);
+// console.log(num3);
+
+// var num4 = Math.floor(5.5);
+// console.log(num4);
+
+// var num5 = Math.ceil(5.5);
+// console.log(num5);
+
+// var num6 = Math.random()*10;
+// var num6 = Math.round(num6);
+// console.log(num6);
+
+// var mangoPrice = 20;
+// if(mangoPrice != 20){
+//     console.log("You can buy mango");
+// }
+// else{
+//     console.log("You can't buy mango");
+// }
+
+
+var friend1Taka = 99;
+var friend2Taka = 60;
+if(friend1Taka == 100 && friend2Taka == 50){
+    console.log("Go to restaturant-01");
+}
+else if(friend1Taka >= 100 || friend2Taka >= 100){
+    console.log("Go to restaturant-02");
+}
+else{
+    console.log("Can't go to restaturant");
+}
+
+
